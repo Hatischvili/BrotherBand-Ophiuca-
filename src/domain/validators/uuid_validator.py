4c8a09fa224a0d8a1829dfbd61c4d09519553038
Clarice Hatischvili <claricehatischvili@gmail.com>
2022-10-20 13:52:12 -0300

    Adiciona validators

---
+++ src/domain/validators/uuid_validator.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from uuid import UUID
+from src.domain.exceptions.domain_validation_error import DomainValidationError
+
+@dataclass
+class UUIDValidator:
+    @staticmethod
+    def validate(value: str, field: str) -> None:
+        DomainValidationError.when(value is None, f"Field {field} cannot be None.")
+        try:
+            _ = UUID(value)
+        except ValueError as exc:
+            raise DomainValidationError(f"Field {field} must be an UUID. [value={value}]") from exc
